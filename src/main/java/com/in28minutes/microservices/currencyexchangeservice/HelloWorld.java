35e04089bfdd71ac94a9c8df699afa26aa39fe0a
sandeep kumar <68270267+sandeepnirania@users.noreply.github.com>
2022-06-03 11:17:38 +0530

    Update HelloWorld.java

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/HelloWorld.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
     public String sayHello(String name) {
 
-        System.out.println("sayHello called update with name : " + name);
+        System.out.println("sayHello called update 1 with name : " + name);
         return "Hello " + name;
     }
 
